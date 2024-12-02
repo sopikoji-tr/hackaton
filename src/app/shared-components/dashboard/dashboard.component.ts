139af456c7954121c1bd9a409ed4bf0fc97c5650
giorgibe-tr <160117990+giorgibe-tr@users.noreply.github.com>
2024-12-02 17:56:35 +0400

    added highliter - prism js

--- src/app/shared-components/dashboard/dashboard.component.ts
+++ src/app/shared-components/dashboard/dashboard.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import * as Prism from 'prismjs';
+import 'prismjs/components/prism-javascript.min.js';  // Add other languages if needed
+
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent {
+export class DashboardComponent implements AfterViewInit{
+
+  ngAfterViewInit() {
+    Prism.highlightAll(); // Highlights all <code> blocks
+  }
 
   items = [
     { title: 'src/app/shared-components/dashboard/dashboard.component.ts', content: 'Content for section 1' },
@@ -13,4 +20,10 @@ export class DashboardComponent {
     { title: 'src/app/shared-components/dashboard/dashboard.component.ts', content: 'Content for section 3' },
   ];
 
+  ngOnInit() {
+    this.items[0].content = `
+    ngAfterViewInit() { 
+        Prism.highlightAll();  // Highlights all <code> blocks
+    }`;
+  }
 }
