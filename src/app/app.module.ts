87454ff2dcc597fc8c070d53969f493f0408907b
sopikoji <sopikoji@etoro.com>
2024-12-03 19:44:36 +0400

    detail

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedComponentsModule } from './shared-components/shared-components.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { MainUrlInterceptor } from './interceptors/main-url.interceptor';
 
 
@@ -17,6 +17,7 @@ import { MainUrlInterceptor } from './interceptors/main-url.interceptor';
     AppRoutingModule,
     BrowserAnimationsModule,
     SharedComponentsModule,
+    HttpClientModule,
   ],
   providers: [
     {
